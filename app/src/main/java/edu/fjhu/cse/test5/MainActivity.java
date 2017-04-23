6c0e32720c47d5eae850abcf6b0082a26baaf7a0
chenshuai <673550207@qq.com>
2017-04-23 10:55:07 +0800

    Initial commit

---
+++ app/src/main/java/edu/fjhu/cse/test5/MainActivity.java
@@ -0,0 +1,35 @@
+package edu.fjhu.cse.test5;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn;
+    private EditText edit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        btn = (Button) findViewById(R.id.btn);
+        edit= (EditText) findViewById(R.id.edit);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(edit.getText().toString()));
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
